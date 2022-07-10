c8489c511eadf6c30d508921912ca91476388c26
WilianPer <wsresilienttasteshopw@gmail.com>
2022-07-10 14:31:50 -0400

    Entendendo JSX!!!

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ export default function Inicio() {
     return (
         <div>
             <h1>Fundamentos de Next.JS & React</h1>
+            <h2>Vamos estudar este FrameWork</h2>
         </div>
     )
 }
