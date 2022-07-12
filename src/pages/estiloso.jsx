ed5e8d089d4a40fae86822992def31181c46c434
WilianPer <wsresilienttasteshopw@gmail.com>
2022-07-12 16:31:28 -0400

    CSS Modularizado!!!

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Estiloso.module.css'
+
+export default function Estiloso() {
+    return (
+        <div className={styles.roxo}>
+            <h1>Estilo usando CSS Módulos</h1>
+        </div>
+        // <div className='roxo'>
+        //     <h1>Estilo usando CSS Módulos</h1>
+        // </div>
+    )
+}
