1b523e295d1ccaa67f9b28912e99d871432ac27e
WilianPer <wsresilienttasteshopw@gmail.com>
2022-07-24 09:08:01 -0400

    Alterando as cores dos botões do inicio!!

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,8 +18,8 @@ export default function Inicio() {
             <Navegador texto="Jsx" destino="/jsx" cor="crimson"/>
             <Navegador texto="Navegação #01" destino="/navegacao/" cor="green"/>
             <Navegador texto="Navegação #02" destino="/cliente/ms-5.0/123" cor="blue"/>
-            <Navegador texto="Componente com Estado" destino="/estado" cor="pink"/>
-            <Navegador texto="Integração com API #01" destino="/integracao_1" cor="yellow"/>
+            <Navegador texto="Componente com Estado" destino="/estado" cor="#a45b71"/>
+            <Navegador texto="Integração com API #01" destino="/integracao_1" cor="#42a9a9"/>
         </div>
     )
 }
